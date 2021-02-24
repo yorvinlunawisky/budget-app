8f643afc4fac79cb9559890a536ac58e600e0e62
Zeus <yorvin0506@gmail.com>
2021-02-24 18:08:57 -0400

    Delete button doesn't work

--- budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { BudgetItem } from 'src/app/shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -7,11 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BudgetItemCardComponent implements OnInit {
   
-  @Input() isIncome: boolean = true;
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onXButtonClick() {
+    //Here we want to emit an event.
+    this.xButtonClick.emit();
+  }
 }
--- budget-app/src/app/main-page/main-page.component.ts
+++ budget-app/src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from '../shared/models/budget-item.model';
 
 @Component({
   selector: 'app-main-page',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  
+  budgetItems: BudgetItem[] = new Array<BudgetItem>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItem(newItem: BudgetItem) {
+  this.budgetItems.push(newItem);
+  }
+
+  deleteItem(item: BudgetItem) {
+    let index = this.budgetItems.indexOf(item);
+    this.budgetItems.splice(index, 1);
+  }
 }
